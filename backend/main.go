0342c49517519f9f0431c1953ef4e85f9b56ff64
mdarify13 <darify1337@gmail.com>
2025-08-28 10:31:44 +0100

    feat: start-go-project

---
+++ backend/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	// Get env variables from docker-compose
+	dbHost := os.Getenv("DATABASE_HOST")
+	dbPort := os.Getenv("DATABASE_PORT")
+	dbUser := os.Getenv("DATABASE_USER")
+	dbPassword := os.Getenv("DATABASE_PASSWORD")
+	dbName := os.Getenv("DATABASE_NAME")
+
+	// Connection string
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		dbHost, dbPort, dbUser, dbPassword, dbName)
+
+	// Connect to Postgres
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		log.Fatal("Failed to connect to DB:", err)
+	}
+	defer db.Close()
+
+	// Simple API route
+	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.Write([]byte(`{"message":"pong"}`))
+	})
+
+	fmt.Println("🚀 Go backend running on port 3000")
+	log.Fatal(http.ListenAndServe(":3000", nil))
+}
